cab963ea38cf49e370d1bf29cb218fd5278722d6
Fred Emmott <fredemmott@fb.com>
2015-02-20 15:55:41 -0800

    aDd top-level init file

---
+++ init.php
@@ -0,0 +1,17 @@
+<?php
+/* Copyright (c) 2015, Facebook, Inc.
+ * All rights reserved.
+ *
+ *  This source code is licensed under the BSD-style license found in the
+ *  LICENSE file in the root directory of this source tree. An additional grant
+ *  of patent rights can be found in the PATENTS file in the same directory.
+ */
+
+// Composer does not support autoloading functions: composer/composer#3683
+require_once(__DIR__.'/ResultOrExceptionWrapper.php');
+require_once(__DIR__.'/WrappedException.php');
+require_once(__DIR__.'/src/WrappedResult.php');
+require_once(__DIR__.'/src/convenience.php');
+require_once(__DIR__.'/src/identities.php');
+require_once(__DIR__.'/src/maps.php');
+require_once(__DIR__.'/src/vectors.php');
